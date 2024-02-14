9c63716791fbb52416659ec80d339e58cc9680cd
tkofb <benard.kihiuria@gmail.com>
2024-02-14 08:35:51 -0500

    Completed todo list portion

---
+++ script.js
@@ -0,0 +1,118 @@
+
+/*
+ TASKLIST SECTION
+ **/
+
+const taskForm = document.getElementById('enterTask');
+const taskList = document.getElementById('taskList');
+const taskItem = document.getElementById('task');
+const checkOffTasks = document.getElementById('checkOffTasks');
+const showTasks = document.getElementById('showCheckedTasks');
+const resetTasks = document.getElementById('resetTasks');
+const items = taskList.getElementsByClassName('checkbox');
+const deleteTasks = taskList.getElementsByClassName('closeTask');
+const listInstances = taskList.getElementsByTagName('li');
+
+let taskAmount = items.length;
+
+function clearTextArea(textarea) {
+  textarea.value = '';
+}
+
+function addTask() {
+  const task = taskItem.value;
+
+  const addition = `<li class=individualTask> \
+          <input type="checkbox" class="checkbox" \ /> \
+          <span>${task}</span> \
+          <button class="closeTask">Close</button> \
+        </li>`;
+  if (task != '') {
+    taskList.innerHTML += addition;
+  }
+
+  return addition;
+}
+
+function removeTask(item) {
+  taskList.removeChild(item);
+}
+
+function removeAllTasks() {
+  let availableIndex = items.length;
+  taskAmount = items.length;
+
+  // doing it this way takes care of data races
+  for (let i = 0; i < taskAmount; i++) {
+    removeTask(listInstances[availableIndex - 1]);
+    availableIndex--;
+  }
+}
+
+function updateButtons() {
+  for (let i = 0; i < deleteTasks.length; i++) {
+    deleteTasks[i].addEventListener('click', function () {
+      removeTask(deleteTasks[i].parentElement);
+    });
+  }
+}
+
+function removeAllCheckedBoxes() {
+  taskAmount = items.length;
+
+  for (let i = 0; i < taskAmount; i++) {
+    if (items[i].checked == true) {
+      listInstances[i].classList.add('hidden');
+    }
+  }
+}
+
+function showHiddenTasks() {
+  taskAmount = items.length;
+
+  for (let i = 0; i < taskAmount; i++) {
+    if (items[i].checked == true) {
+      listInstances[i].classList.remove('hidden');
+    }
+  }
+}
+
+checkOffTasks.addEventListener('click', function (e) {
+  e.preventDefault();
+  removeAllCheckedBoxes();
+});
+
+showCheckedTasks.addEventListener('click', function (e) {
+  e.preventDefault();
+  showHiddenTasks();
+});
+
+resetTasks.addEventListener('click', function (e) {
+  e.preventDefault();
+  removeAllTasks();
+});
+
+taskForm.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    addTask();
+    clearTextArea(taskItem);
+  }
+
+  updateButtons();
+});
+
+taskForm.addEventListener('submit', function (e) {
+  e.preventDefault(); //To prevent the button from autosubmitting
+  addTask();
+  clearTextArea(taskItem);
+
+  updateButtons();
+});
+
+
+/*
+ WEATHER SECTION
+ **/
+
+
